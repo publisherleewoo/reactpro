5b891d90efdef6b7fc93e50e030fb25936b34ef3
Lee_woo <dlguddn3214@naver.com>
2020-07-12 14:29:43 +0900

    chapter5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-import "./App.css";
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+import { SportsStoreDataStore } from "./data/DataStore";
+import { Provider } from "react-redux";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from "react-router-dom";
+import { ShopConnector } from "./shop/ShopConnector";
+export default class App extends Component {
     render() {
-        return <div></div>;
+        return (
+            <Provider store={SportsStoreDataStore}>
+                <Router>
+                    <Switch>
+                        <Route path="/shop" component={ShopConnector} />
+                        <Redirect to="/shop" />
+                    </Switch>
+                </Router>
+            </Provider>
+        );
     }
 }
-
-export default App;
